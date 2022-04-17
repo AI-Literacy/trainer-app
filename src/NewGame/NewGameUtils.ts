5cbfd28ffc1a90bcf49bdbd1b8d850cf361aa696
Parth Sarin <psarin@stanford.edu>
2022-04-17 13:28:22 -0700

    decomposition & error handling

--- src/NewGame/NewGameUtils.ts
+++ src/NewGame/NewGameUtils.ts
@@ -1,5 +1,8 @@
 import { doc, getDoc, getFirestore, serverTimestamp, setDoc } from "firebase/firestore";
 import { GameTemplate } from "./Template";
+import swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const MySwal = withReactContent(swal);
 
 export function validateGameCodeStructure(newGameCode: string) {
   // Long enough
@@ -30,25 +33,45 @@ export async function validateGameCode(newGameCode: string) {
 }
 
 
-export async function makeNewGame(
-  template: GameTemplate,
-  uid: string
-): Promise<boolean> {
+function error(msg: string) {
+  MySwal.fire({
+    title: 'Error',
+    text: msg,
+    icon: 'error',
+  })
+  return false;
+}
+
+
+async function validateTemplate(template: GameTemplate) {
   const { gameCode, started, viewsPerCard, cardsPerPlayer, fields, question } = template;
 
   // Validate parameters
   const gameCodeError = await validateGameCode(gameCode);
-  if (gameCodeError) return false;
-  if (!(1 <= viewsPerCard && viewsPerCard <= 100)) return false;
-  if (!(1 <= cardsPerPlayer && cardsPerPlayer <= 10)) return false;
+  if (gameCodeError) return error(gameCodeError);
+  if (!(1 <= viewsPerCard && viewsPerCard <= 100)) return error("Number of views per card must be between 1 and 100.");
+  if (!(1 <= cardsPerPlayer && cardsPerPlayer <= 10)) return error("Number of cards per player must be between 1 and 10.");
   const minMaxValid = Object.values(fields)
     .map(field => field.min < field.max)
     .reduce((a, b) => a && b, true);
-  if (!minMaxValid) return false;
-  if (started) return false;
-  if (!question) return false;
+  if (!minMaxValid) return error("Min value must be less than max value.");
+  if (started) return error("Game already started.");
+  if (!question) return error("Please enter a question.");
+
+  return true;
+}
+
+
+export async function makeNewGame(
+  template: GameTemplate,
+  uid: string
+): Promise<boolean> {
+  const valid = await validateTemplate(template);
+  if (!valid) return false;
 
   // Push the game to the database
+  const { gameCode, fields } = template;
+
   // 1. Change user's active game
   const db = getFirestore();
   await setDoc(
@@ -66,10 +89,7 @@ export async function makeNewGame(
   delete docVal['gameCode'];
   delete docVal['fields'];
 
-  await setDoc(
-    doc(db, 'games', gameCode),
-    docVal
-  );
+  await setDoc(doc(db, 'games', gameCode), docVal);
 
   // 3. Push the game fields to the database
   await setDoc(
