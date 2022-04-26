3207a0a6667495efb89f8068a0fcd21f6cc08865
Isabel Sieh <isabelsieh@DN51vt5r.SUNet>
2022-04-26 09:20:56 -0700

    generate card data

--- src/NewGame/NewGameUtils.ts
+++ src/NewGame/NewGameUtils.ts
@@ -1,5 +1,5 @@
 import { doc, getDoc, getFirestore, serverTimestamp, setDoc } from "firebase/firestore";
-import { GameTemplate } from "./Template";
+import { GameField, GameTemplate } from "./Template";
 import swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 const MySwal = withReactContent(swal);
@@ -70,8 +70,12 @@ export async function makeNewGame(
   if (!valid) return false;
 
   // Push the game to the database
-  const { gameCode, fields } = template;
+  const { gameCode, question, viewsPerCard, 
+    cardsPerPlayer, fields, started} = template;
+  console.log(fields);
+  const cards = generateCards(25, viewsPerCard, cardsPerPlayer, fields)
 
+  // calculate number of cards
   // 1. Change user's active game
   const db = getFirestore();
   await setDoc(
@@ -98,4 +102,115 @@ export async function makeNewGame(
   )
 
   return true;
+}
+
+function generateCards(numStudents: number, viewsPerCard: number, cardsPerPlayer: number, fields: any){
+  if (numStudents < viewsPerCard){
+    viewsPerCard = numStudents;
+  }
+  let numCards = Math.floor((cardsPerPlayer * numStudents)/ viewsPerCard);
+
+  // METHOD 1:
+  // * populate a card with random values for each field
+  // * if that value combination is the same as another card, redo
+  //
+  // let cards = new Array(0);
+
+  // for (let i = 0; i < numCards; i++){
+  //   let newCard = Object.create(null);
+  //   let unique = false;
+  //   while (!unique){
+  //     for(const field of Object.keys(fields)){
+  //       newCard[field] = getRandomInt(fields[field]['min'], fields[field]['max']);
+  //     }
+  //     unique = true;
+  //     for(const card of cards){
+  //       if (cardsEqual(newCard,card, fields)){
+  //         unique = false;
+  //         break;
+  //       }
+  //     }
+  //   }
+  //   cards.push(newCard)
+  // }
+
+  // METHOD 2:
+  // * give each card random value for field 1
+  // * go to next field
+  // * for each card:
+  //  * check for each pair with previous fields that 
+  //    the value pair is different from past cards
+  //  * if the value pair is the same, redo value for that field 
+  //  * repeat last 2 steps for all fields
+
+  let cards = new Array(numCards);
+  for (let c = 0; c <numCards; c++){
+    cards[c] = Object.create(null)
+  } 
+
+  let fieldNum = 0; // track field # being added
+
+  for(const field of Object.keys(fields)){
+    let cardNum = 0; // track card # being edited
+    console.log(field);
+    for (let c = 0; c <numCards; c++){
+
+      // repeate until a unique number is found 
+      let unique = false;
+      while(!unique){
+        // pick random value
+        cards[c][field] = getRandomInt(fields[field]['min'], fields[field]['max']);
+        unique = true;
+
+        // goes through each field that field could pair with
+        for(let i = 0; i < fieldNum; i++){
+          // looks at all pairs of previous cards and checks if equal
+          if(pairEqual(cards[c], cards, cardNum, i, fieldNum, fields)){
+            unique = false; // if the pair is equal, try a new value
+          }
+        }        
+      }
+
+      cardNum ++;
+    }
+
+    fieldNum ++;
+  }
+  
+  console.log(cards);
+  return cards;
+}
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max)+1;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function cardsEqual(newCard: any, card: any, fields: any){
+  for (const field of Object.keys(fields)){
+    if (newCard[field] != card[field]){
+      return false;
+    }
+  }
+  console.log("MATCH")
+  console.log(newCard, card)
+  return true;
+}
+
+function pairEqual(card: any, cards: any, cardNum: number, 
+  pastField: number, curField: number, fields: any){
+  
+  let pastFieldId = Object.keys(fields)[pastField];
+  let curFieldId = Object.keys(fields)[curField];
+
+  for(let i = 0; i < cardNum; i++){
+    if ((card[pastFieldId] === cards[i][pastFieldId]) 
+      && (card[curFieldId] === cards[i][curFieldId])){
+      console.log("MATCH");
+      console.log(card, cards[i])
+      return true;
+    }
+  }
+  return false;
 }
