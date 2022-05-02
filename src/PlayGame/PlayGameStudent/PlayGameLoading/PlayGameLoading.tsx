8605495be3a3cab1f36ef6aecfa5c5115d71185a
Isabel Sieh <isabelsieh@DN51vsh3.SUNet>
2022-05-02 00:11:52 -0700

    loading animation for players

--- src/PlayGame/PlayGameStudent/PlayGameLoading/PlayGameLoading.tsx
+++ src/PlayGame/PlayGameStudent/PlayGameLoading/PlayGameLoading.tsx
@@ -3,6 +3,8 @@ import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../../../App";
 import { addSelfToGame } from "./StudentLoadingUtils";
+import { HashLoader } from 'react-spinners';
+
 
 const PlayGameLoading = () => {
   const { gid } = useParams();
@@ -13,7 +15,15 @@ const PlayGameLoading = () => {
     addSelfToGame(gid, user);    
   }, [gid, user]);
 
-  return (<p>Loading</p>);
+  return (
+    <div className='fixed w-full h-full flex align-center z-50 bg-black bg-opacity-50'>
+        <div className="flex w-full h-full justify-center">
+            <div className="flex self-center">
+                <HashLoader color={'white'} size={150} />
+            </div>
+        </div>
+    </div>
+  );
 };
 
 export default PlayGameLoading;
